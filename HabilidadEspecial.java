5d53b27ab82d5fcf27853e27d6bd5474bcf7547d
daniMont06 <danimontenegrodelaroca@gmail.com>
2025-09-28 18:17:22 -0600

    :D

---
+++ HabilidadEspecial.java
@@ -0,0 +1,46 @@
+import java.util.Random;
+public class HabilidadEspecial{
+    private String nombre;
+    private int ataque;        
+    private boolean usada;    
+
+    private Random rng = new Random();
+
+    public HabilidadEspecial(String nombre, int ataque){
+        this.nombre = nombre;
+        this.ataque = ataque;
+        this.usada = false;
+    }
+
+    public boolean activar(){
+        if (usada) return false;
+        return rng.nextBoolean(); 
+    }
+
+    public void uso(Enemigo enemigo){
+        if (enemigo == null) return;
+        if (usada) return;
+
+        enemigo.subir_ataque(ataque);
+        usada = true;
+    }
+
+    public void ataque(Jugador jugador, Enemigo enemigo){
+        if (enemigo == null) return;
+        if (usada) return;
+
+        enemigo.subir_ataque(ataque);
+        usada = true;
+    }
+
+    //Getters y Setters
+    public String getNombre(){ 
+        return nombre; 
+        }
+    public int getAtaque(){ 
+        return ataque; 
+        }
+    public boolean yaFueUsada(){ 
+        return usada; 
+        }
+}
